629e78cd4f1e198c0f691bea60bd0647d4b0db08
pnezis <pnezis@gmail.com>
2023-08-12 18:38:47 +0300

    refactor datasets

--- lib/tucan/datasets.ex
+++ lib/tucan/datasets.ex
@@ -15,31 +15,52 @@ defmodule Tucan.Datasets do
           | :flights
           | :tips
 
-  def dataset(:iris),
-    do:
-      "https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv"
+  @datasets [
+    iris: [
+      url:
+        "https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv"
+    ],
+    corruption: [
+      url:
+        "https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/corruption.csv"
+    ],
+    cars: [
+      url: "https://vega.github.io/editor/data/cars.json"
+    ],
+    gapminder: [
+      url: "https://vega.github.io/vega-datasets/data/gapminder-health-income.csv"
+    ],
+    weather: [
+      url: "https://vega.github.io/editor/data/weather.csv"
+    ],
+    stocks: [
+      url: "https://vega.github.io/editor/data/stocks.csv"
+    ],
+    titanic: [
+      url: "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+    ],
+    penguins: [
+      url: "https://raw.githubusercontent.com/vega/vega-datasets/next/data/penguins.json"
+    ],
+    flights: [
+      url: "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/flights.csv"
+    ],
+    tips: [url: "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"]
+  ]
 
-  def dataset(:corruption),
-    do:
-      "https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/corruption.csv"
+  @valid_datasets @datasets
+                  |> Keyword.keys()
+                  |> Enum.sort()
 
-  def dataset(:cars), do: "https://vega.github.io/editor/data/cars.json"
+  @spec dataset(atom()) :: binary()
+  def dataset(dataset) when is_atom(dataset) do
+    case @datasets[dataset] do
+      nil ->
+        raise ArgumentError,
+              "not supported dataset #{inspect(dataset)}, valid datasets: #{inspect(@valid_datasets)}"
 
-  def dataset(:gapminder),
-    do: "https://vega.github.io/vega-datasets/data/gapminder-health-income.csv"
-
-  def dataset(:weather), do: "https://vega.github.io/editor/data/weather.csv"
-
-  def dataset(:stocks), do: "https://vega.github.io/editor/data/stocks.csv"
-
-  def dataset(:titanic),
-    do: "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
-
-  def dataset(:penguins),
-    do: "https://raw.githubusercontent.com/vega/vega-datasets/next/data/penguins.json"
-
-  def dataset(:flights),
-    do: "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/flights.csv"
-
-  def dataset(:tips), do: "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"
+      dataset ->
+        Keyword.fetch!(dataset, :url)
+    end
+  end
 end
