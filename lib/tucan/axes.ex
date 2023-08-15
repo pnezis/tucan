10f28070fdb45b5cb02abdbd4f580f58e1069ee4
pnezis <pnezis@gmail.com>
2023-08-15 20:28:31 +0300

    refactor set_title in axes

--- lib/tucan/axes.ex
+++ lib/tucan/axes.ex
@@ -4,24 +4,36 @@ defmodule Tucan.Axes do
   """
   alias Tucan.VegaLiteUtils
 
+  @type axis :: :x | :y
+
   @doc """
   Sets the x axis title.
 
-  An `ArgumentError` is raised if the `x` encoding channel is not defined.
+  This is an alias for `set_title/3`.
   """
   @spec set_x_title(vl :: VegaLite.t(), title :: binary()) :: VegaLite.t()
   def set_x_title(vl, title) when is_struct(vl, VegaLite) and is_binary(title) do
-    put_axis_options(vl, :x, title: title)
+    set_title(vl, :x, title)
   end
 
   @doc """
   Sets the y axis title.
 
-  An `ArgumentError` is raised if the `x` encoding channel is not defined.
+  This is an alias for `set_title/3`.
   """
   @spec set_y_title(vl :: VegaLite.t(), title :: binary()) :: VegaLite.t()
   def set_y_title(vl, title) when is_struct(vl, VegaLite) and is_binary(title) do
-    put_axis_options(vl, :y, title: title)
+    set_title(vl, :y, title)
+  end
+
+  @doc """
+  Set the title of the given `axis`.
+
+  An `ArgumentError` is raised if the `x` encoding channel is not defined.
+  """
+  @spec set_title(vl :: VegaLite.t(), axis :: axis(), title :: binary()) :: VegaLite.t()
+  def set_title(vl, axis, title) do
+    put_axis_options(vl, axis, title: title)
   end
 
   defp put_axis_options(vl, encoding, options) do
