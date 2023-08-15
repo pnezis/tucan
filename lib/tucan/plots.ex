b10c20d317f70a4e46c1fdf4518eb1c2d4c85bc3
pnezis <pnezis@gmail.com>
2023-08-15 20:29:32 +0300

    remove deprecated code

--- lib/tucan/plots.ex
+++
@@ -1,7 +0,0 @@
-defmodule Tucan.Plots do
-  # def add_marginal_histogram(plot, field, opts) do
-  #   histogram = histogram(Vl.new(), field, opts)
-  #   plot = Vl.new() |> Vl.data(plot.spec["data"])
-  #   Vl.concat(Vl.new(), [histogram, plot], :vertical)
-  # end
-end
