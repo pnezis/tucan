999f37def490253e56032e5e0cdaf14a74e8f8fd
pnezis <pnezis@gmail.com>
2023-08-26 14:47:39 +0300

    add missing specs

--- lib/tucan/keyword.ex
+++ lib/tucan/keyword.ex
@@ -4,6 +4,12 @@ defmodule Tucan.Keyword do
   # Helper keyword utility functions
 
   @doc false
+  @spec put_new_conditionally(
+          keywords :: keyword(),
+          key :: atom(),
+          value :: term(),
+          fun :: (() -> boolean())
+        ) :: keyword()
   def put_new_conditionally(keywords, key, value, fun) do
     cond do
       Keyword.has_key?(keywords, key) ->
@@ -18,6 +24,7 @@ defmodule Tucan.Keyword do
   end
 
   @doc false
+  @spec deep_merge(config1 :: keyword(), config2 :: keyword()) :: keyword()
   def deep_merge(config1, config2) when is_list(config1) and is_list(config2) do
     Keyword.merge(config1, config2, fn _, value1, value2 ->
       if Keyword.keyword?(value1) and Keyword.keyword?(value2) do
