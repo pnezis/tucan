3ab698c4ee797bba84f78fef1b2358f9595d9431
pnezis <pnezis@gmail.com>
2023-12-03 21:41:00 +0200

    add tests for polyline and rectangle

--- test/tucan/geometry_test.exs
+++ test/tucan/geometry_test.exs
@@ -54,4 +54,91 @@ defmodule Tucan.GeometryTest do
              }
     end
   end
+
+  describe "polyline/3" do
+    @points [{1, 2}, {5, 7}, {9, 13}, {-1, 4}]
+
+    test "with default settings" do
+      expected =
+        Vl.new()
+        |> Vl.layers([
+          Vl.new()
+          |> Vl.mark(:line, stroke_width: 1, fill_opacity: 1, stroke_opacity: 1)
+          |> Vl.data_from_values(%{x: [1, 5, 9, -1], y: [2, 7, 13, 4], order: [0, 1, 2, 3]})
+          |> Vl.encode_field(:x, "x", type: :quantitative)
+          |> Vl.encode_field(:y, "y", type: :quantitative)
+          |> Vl.encode_field(:order, "order")
+        ])
+
+      assert Tucan.Geometry.polyline(Vl.new(), @points) == expected
+    end
+
+    test "with closed set to true" do
+      expected =
+        Vl.new()
+        |> Vl.layers([
+          Vl.new()
+          |> Vl.mark(:line, stroke_width: 1, fill_opacity: 1, stroke_opacity: 1)
+          |> Vl.data_from_values(%{
+            x: [1, 5, 9, -1, 1],
+            y: [2, 7, 13, 4, 2],
+            order: [0, 1, 2, 3, 4]
+          })
+          |> Vl.encode_field(:x, "x", type: :quantitative)
+          |> Vl.encode_field(:y, "y", type: :quantitative)
+          |> Vl.encode_field(:order, "order")
+        ])
+
+      assert Tucan.Geometry.polyline(Vl.new(), @points, closed: true) == expected
+    end
+
+    test "with styling options" do
+      %VegaLite{spec: %{"layer" => [polygon]}} =
+        Tucan.Geometry.polyline(Vl.new(), @points,
+          line_color: "red",
+          stroke_width: 4,
+          fill_color: "green",
+          fill_opacity: 0.2,
+          stroke_opacity: 0.5,
+          stroke_dash: [5, 5]
+        )
+
+      assert polygon["mark"] == %{
+               "strokeWidth" => 4,
+               "type" => "line",
+               "color" => "red",
+               "fillOpacity" => 0.2,
+               "fill" => "green",
+               "strokeOpacity" => 0.5,
+               "strokeDash" => [5, 5]
+             }
+    end
+  end
+
+  describe "rectangle/4" do
+    test "raises with invalid points" do
+      assert_raise ArgumentError, "the two points must have different x coordinates", fn ->
+        Tucan.Geometry.rectangle(Vl.new(), {1, 1}, {1, 3})
+      end
+
+      assert_raise ArgumentError, "the two points must have different y coordinates", fn ->
+        Tucan.Geometry.rectangle(Vl.new(), {1, 3}, {2, 3})
+      end
+    end
+
+    test "valid rectangle" do
+      expected =
+        Vl.new()
+        |> Vl.layers([
+          Vl.new()
+          |> Vl.mark(:line, stroke_width: 1, fill_opacity: 1, stroke_opacity: 1)
+          |> Vl.data_from_values(%{x: [1, 1, 5, 5, 1], y: [2, 6, 6, 2, 2], order: [0, 1, 2, 3, 4]})
+          |> Vl.encode_field(:x, "x", type: :quantitative)
+          |> Vl.encode_field(:y, "y", type: :quantitative)
+          |> Vl.encode_field(:order, "order")
+        ])
+
+      assert Tucan.Geometry.rectangle(Vl.new(), {1, 2}, {5, 6}) == expected
+    end
+  end
 end
