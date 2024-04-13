005c0ce0e9653cea39a9b24fb65788dab3c1207a
pnezis <pnezis@gmail.com>
2024-04-13 20:43:43 +0300

    Bump dependencies

--- mix.exs
+++ mix.exs
@@ -38,11 +38,11 @@ defmodule Tucan.MixProject do
   defp deps do
     [
       {:nimble_options, "~> 1.1"},
-      {:vega_lite, "~> 0.1.8"},
+      {:vega_lite, "~> 0.1.9"},
       {:jason, "~> 1.4"},
       {:nx, "~> 0.6", optional: true},
-      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
-      {:fancy_fences, "~> 0.3.0", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.32", only: :dev, runtime: false},
+      {:fancy_fences, "~> 0.3.1", only: :dev, runtime: false}
     ] ++ dev_deps()
   end
 
